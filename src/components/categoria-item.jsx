8cd7069c0348691aed5c7bfaa23df67b7cf1d872
Luciano Silva <luciano_jet@hotmail.com>
2024-04-03 09:08:04 -0300

    colocado rotulos dinamicos

--- src/components/categoria-item.jsx
+++ src/components/categoria-item.jsx
@@ -1,11 +1,11 @@
 import { Badge } from "./ui/badge";
 import { Pizza } from 'lucide-react';
 
-const CategoriaItem = ({categoria}) => {
+const CategoriaItem = ({ categoria, label }) => {
     return (
         <Badge variant="outline" className="flex justify-center align-items-center p-3 gap-2 rounded-lg bg-secondary-foreground text-primary">
             <Pizza/>
-            <span className="font-bold text-xs">Pizza Tradicional</span>
+            <span className="font-bold text-xs">{label}</span>
         </Badge>
     );
 }
--- src/components/categorias.jsx
+++ src/components/categorias.jsx
@@ -1,14 +1,13 @@
 // esse componente precisa pegar as categorias do banco de dados
-
 import CategoriaItem from "./categoria-item";
 
 const Categorias = () => {
     return (
         <div className="grid grid-cols-2 gap-y-2 gap-x-4 mt-5">
-            <CategoriaItem />
-            <CategoriaItem />
-            <CategoriaItem />
-            <CategoriaItem />
+            <CategoriaItem label="Pizza Tradicional" />
+            <CategoriaItem label="Lanches" />
+            <CategoriaItem label="Bebidas" />
+            <CategoriaItem label="Sobremesas" />
         </div>
     );
 }
