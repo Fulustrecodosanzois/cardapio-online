4a77cb6ad564e5008b3f3bd82b7be9986a10c4f8
Luciano Silva <luciano_jet@hotmail.com>
2024-04-08 14:49:10 -0300

    voltado ao layout anterior

--- src/components/carousel-items.jsx
+++ src/components/carousel-items.jsx
@@ -5,8 +5,8 @@ import { Card } from "./ui/card";
 
 const CarouselItems = ({ produto }) => {
     return (
-        <Card className="flex flex-col border-hidden bg-transparent">
-            <div className="w-[167px] h-[170px] rounded-lg ">
+        <Card className="flex flex-col border-hidden">
+            <div className="w-[167px] h-[250px] rounded-lg ">
                 <div className="flex align-items-center justify-center rounded-tl-lg gap-3 rounded-tr-lg bg-secondary-foreground w-[167px] h-[170px]">
                     <Image
                         src={produto.imgUrl}
@@ -17,11 +17,11 @@ const CarouselItems = ({ produto }) => {
                         className="object-contain"
                     />
                 </div>
-            </div>
-            <div className="rounded-lg flex flex-col justify-between h-[75px] px-3">
-                <div className="flex justify-between flex-col mt-2">
-                    <p className="text-sm font-semibold truncate">{produto.Nome}</p>
-                    <p className="text-lg text-end font-semibold text-red-600 mt-3">R$ {produto.PrecoF}</p>
+                <div className="rounded-lg flex flex-col justify-between h-[75px] px-3">
+                    <div className="flex justify-between flex-col mt-2">
+                        <p className="text-sm font-semibold truncate">{produto.Nome}</p>
+                        <p className="text-lg text-end font-semibold text-red-600 mt-3">R$ {produto.PrecoF}</p>
+                    </div>
                 </div>
             </div>
         </Card>
