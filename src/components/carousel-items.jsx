7b779d5d6d8703e6605aba92aaa8bfaf7dc80e7d
Luciano Silva <luciano_jet@hotmail.com>
2024-04-08 10:32:55 -0300

    ajustado precos e nomes

--- src/components/carousel-items.jsx
+++ src/components/carousel-items.jsx
@@ -6,7 +6,7 @@ import { Card } from "./ui/card";
 const CarouselItems = ({ produto }) => {
     return (
         <Card className="flex flex-col border-hidden">
-            <div className="w-[167px] h-[228px] rounded-lg ">
+            <div className="w-[167px] h-[250px] rounded-lg ">
                 <div className="flex align-items-center justify-center rounded-tl-lg gap-3 rounded-tr-lg bg-secondary-foreground w-[167px] h-[170px]">
                     <Image
                         src={produto.imgUrl}
@@ -18,9 +18,9 @@ const CarouselItems = ({ produto }) => {
                     />
                 </div>
                 <div className="rounded-lg bg-accent flex flex-col justify-between h-[58px] px-3">
-                    <div className="flex justify-between mt-7">
-                        <p className="text-sm mt-1 text-gray-600 font-semibold">Valor:</p>
-                        <p className="text-lg font-semibold text-red-600">R$ {produto.PrecoF}</p>
+                    <div className="flex justify-between flex-col mt-2">
+                        <p className="text-sm font-semibold truncate">{produto.Nome}</p>
+                        <p className="text-lg text-end font-semibold text-red-600 mt-3">R$ {produto.PrecoF}</p>
                     </div>
                 </div>
             </div>
