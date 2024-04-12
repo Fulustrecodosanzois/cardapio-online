f026df28c3a4bea51be3faf109ed65730e00fac4
Luciano Silva <luciano_jet@hotmail.com>
2024-04-12 13:22:25 -0300

    ajustado btn de retirada e entrega

--- src/components/btn-finalizarpedido.jsx
+++ src/components/btn-finalizarpedido.jsx
@@ -4,10 +4,10 @@ const ButtonFinalizar = () => {
 
     return (
 
-        <div className="my-5 p-2">
+        <div className="my-auto p-2">
             <Button
                 type="submit"
-                className="bg-primary py-2 w-full rounded-md focus:outline-none mr-7 text-white font-mono shadow-lg font-bold" >Finalizar Pedido</Button>
+                className="bg-primary py-2 w-full rounded-md focus:outline-none text-white font-mono shadow-lg font-bold" >Finalizar Pedido</Button>
         </div>
 
     )
