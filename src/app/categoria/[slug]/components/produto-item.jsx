f98decaccd892e8f28d9b7a78f78221f0d081761
glaydson-alves <glaydson.alves36@gmail.com>
2024-04-30 14:21:50 -0300

    style: ajuste bordas de produto-item

--- src/app/categoria/[slug]/components/produto-item.jsx
+++ src/app/categoria/[slug]/components/produto-item.jsx
@@ -49,9 +49,9 @@ const ProdutoItem = ({ produto, preco, categoriaSelecionada, tamanhoSelecionado 
 
     return (
         <Link href={`/produto/${produto.Slug}`}>
-            <Card className="flex flex-col border-hidden p-[2px]">
-                <div className="w-[167px] h-[228px] rounded-lg">
-                    <div className="flex align-items-center  justify-center rounded-lg gap-3 bg-secondary-foreground w-[167px] h-[170px]">
+            <Card className="flex flex-col border-hidden p-[4px]">
+                <div className="flex justify-center flex-col w-[167px] h-[228px] rounded-lg">
+                    <div className="flex align-items-center  justify-center rounded-lg gap-3 bg-secondary-foreground w-[160px] h-[170px]">
                         <Image
                             src={produto.imgUrl}
                             alt={produto.Nome}
@@ -63,7 +63,7 @@ const ProdutoItem = ({ produto, preco, categoriaSelecionada, tamanhoSelecionado 
                     </div>
                     <div className="rounded-lg bg-accent flex flex-col align-items-center justify-center h-[58px]">
                         <p className="text-left px-3 text-sm font-semibold truncate">{produto.Nome}</p>
-                        <div className="flex justify-between items-baseline pl-2">
+                        <div className="flex justify-between items-baseline px-2">
                             <p className="text-left font-bold text-secondary">{precoFormatado}</p>
                             <Button className="bg-accent border-primary" size="icon" variant="outline">
                                 <ShoppingCart className="text-primary" />
